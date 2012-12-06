700ae97abfb4618c615f1b9605471d430f2a9c01
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-12-06 20:39:01 +0100

    replace GenerationEnumerator w/ yield return

--- ConwaysGameOfLifeKatas.Console/Generations.cs
+++ ConwaysGameOfLifeKatas.Console/Generations.cs
@@ -16,7 +16,14 @@ namespace ConwaysGameOfLifeKatas.Console
 
         public IEnumerator<Generation> GetEnumerator()
         {
-            return new GenerationEnumerator(seedGeneration);
+            var nextGeneration = seedGeneration.Tick();
+
+            while (true)
+            {
+                yield return nextGeneration;
+
+                nextGeneration = nextGeneration.Tick();
+            }
         }
 
         public Generation this[int index]
@@ -27,36 +34,6 @@ namespace ConwaysGameOfLifeKatas.Console
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
-        }
-
-        public class GenerationEnumerator : IEnumerator<Generation>
-        {
-            public GenerationEnumerator(Generation generation)
-            {
-                Current = generation;
-            }
-
-            public void Dispose()
-            {
-            }
-
-            public bool MoveNext()
-            {
-                Current = Current.Tick();
-
-                return true;
-            }
-
-            public void Reset()
-            {
-            }
-
-            public Generation Current { get; private set; }
-
-            object IEnumerator.Current
-            {
-                get { return Current; }
-            }
         }
     }
 }
