028843b7f2057341f2f00092716cd1f0069ebab9
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-12-08 16:24:00 +0100

    introduce Cell w/ equality test

---
+++ ConwaysGameOfLifeKatas.Console/Cell.cs
@@ -0,0 +1,32 @@
+﻿namespace ConwaysGameOfLifeKatas.Console
+{
+    public class Cell
+    {
+        private readonly int x;
+        private readonly int y;
+
+        public Cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        protected bool Equals(Cell other)
+        {
+            return x == other.x && y == other.y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Cell);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (x * 397) ^ y;
+            }
+        }
+    }
+}
---
+++ ConwaysWayOfLifeKatas.UnitTests/CellTests.cs
@@ -0,0 +1,19 @@
+﻿using ConwaysGameOfLifeKatas.Console;
+using FluentAssertions;
+using Xunit;
+
+namespace ConwaysWayOfLifeKatas.UnitTests
+{
+    public class CellTests
+    {
+        [Fact]
+        public void cells_should_be_value_equal()
+        {
+            var a23Cell = new Cell(2, 3);
+            var another23Cell = new Cell(2, 3);
+
+            a23Cell.Should().Be(another23Cell);
+            a23Cell.GetHashCode().Should().Be(another23Cell.GetHashCode());
+        }
+    }
+}
